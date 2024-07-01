3c7631309772d54301d326ff3603f15f4ce9e40d
alfital2 <alfital2@users.noreply.github.com>
2024-07-01 13:23:29 +0000

    deploy: 277da8b848994b1dba5896c318e43213a8177c33

--- scripts.js
+++ scripts.js
@@ -5,18 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const loadingDiv = document.getElementById('loading');
   const fileInput = document.getElementById('images');
   const fileChosen = document.getElementById('file-chosen');
-
+  const imageOrientationInput = document.getElementById('imageOrientation'); // Hidden input for orientation
   const textInput = document.getElementById('text');
   const textValidation = document.getElementById('text-validation');
 
-  const backendURL = "https://boomermoviemaker-backend.onrender.com"
-  //const backendURL = "http://localhost:3000"
+  const backendURL = "https://boomermoviemaker-backend.onrender.com";
+  //const backendURL = "http://localhost:3000";
 
   textInput.addEventListener('input', () => {
     const value = textInput.value;
     const hebrewRegex = /^[\u0590-\u05FF\s0-9!@#$%^&*,'()_+-=]*$/;
 
-    if (value.length > 30 || !hebrewRegex.test(value)) {
+    if (value.length > 20 || !hebrewRegex.test(value)) {
       textValidation.style.display = 'block';
     } else {
       textValidation.style.display = 'none';
@@ -25,7 +25,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
   fileInput.addEventListener('change', () => {
     if (fileInput.files.length > 0) {
-      fileChosen.textContent = Array.from(fileInput.files).map(file => file.name).join(', ');
+      const file = fileInput.files[0];
+      fileChosen.textContent = file.name;
+
+      // Read image dimensions
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        console.log(`Image dimensions: width = ${img.width}, height = ${img.height}`);
+        if (img.width < img.height) {
+          imageOrientationInput.value = 'portrait';
+        } else {
+          imageOrientationInput.value = 'landscape';
+        }
+        console.log(`Image orientation: ${imageOrientationInput.value}`);
+        URL.revokeObjectURL(img.src); // Clean up object URL
+      };
     } else {
       fileChosen.textContent = 'לא נבחרו קבצים';
     }
@@ -42,25 +57,21 @@ document.addEventListener('DOMContentLoaded', () => {
       return response.json();
     })
     .then(data => {
-      console.log('Options fetched:', data);
       populateSelect('song', data.songs);
       populateSelect('animation', data.animations);
       populateSelect('background', data.backgrounds);
     })
     .catch(error => {
-      console.error('Error fetching options:', error);
       alert('Failed to load options. Please refresh the page.');
     });
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    console.log('trying to submit');
 
     if (fileInput.files.length === 0) {
       alert('נא לבחור תמונה');
       return;
     }
-
 
     if (textValidation.style.display === 'block') {
       alert('נא לתקן את הטעויות בטקסט לפני שליחת הטופס.');
@@ -70,6 +81,8 @@ document.addEventListener('DOMContentLoaded', () => {
     loadingDiv.style.display = 'block'; // Show loading message
 
     const formData = new FormData(form);
+    // Append image orientation to the form data
+    formData.append('imageOrientation', document.getElementById('imageOrientation').value);
 
     try {
       console.log('Sending request to create video...');
@@ -89,7 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       if (result.status === 'complete') {
         console.log('Video created:', result);
-        downloadLink.href = `${backendURL}/${result.videoUrl}`
+        downloadLink.href = `${backendURL}${result.videoUrl}`;
         resultDiv.style.display = 'block';
         console.log('Result displayed');
       } else {
@@ -105,6 +118,30 @@ document.addEventListener('DOMContentLoaded', () => {
       loadingDiv.style.display = 'none'; // Hide loading message
     }
   });
+
+  downloadLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    downloadVideo(downloadLink.href);
+  });
+
+  function downloadVideo(url) {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        const blobUrl = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = blobUrl;
+        a.download = 'boomer_movie.mp4';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(blobUrl);
+      })
+      .catch(error => {
+        console.error('Download failed:', error);
+        alert('Failed to download the video. Please try again.');
+      });
+  }
 
   function populateSelect(id, options) {
     const select = document.getElementById(id);
